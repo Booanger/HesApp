827fa849169e3d6933c3040a7d7a0dd8b3839e72
Booanger <kerndo32@gmail.com>
2023-05-26 01:54:39 +0300

    Add CORS support to the Flask application

--- app/__init__.py
+++ app/__init__.py
@@ -3,8 +3,10 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
 from flask_migrate import Migrate
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 jwt = JWTManager(app)
