f687ee1668167bf8c40e2ccfb59f755a44ade428
Booanger <kerndo32@gmail.com>
2023-06-11 11:38:02 +0300

    Clean up app init file

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,9 @@ from flask_jwt_extended import JWTManager
 from flask_migrate import Migrate
 from flask_cors import CORS
 from flask_restx import Api
+
 from .models import db
+from .routes import auth_routes, user_routes, restaurant_routes, menu_routes, table_routes
 
 app = Flask(__name__)
 CORS(app)
@@ -14,26 +16,20 @@ db.init_app(app)
 jwt = JWTManager(app)
 migrate = Migrate(app, db)
 
-authorizations = {
-    'Bearer Auth': {
-        'type': 'apiKey',
-        'in': 'header',
-        'name': 'Authorization'
+api = Api(
+    app,
+    version='1.0',
+    title='HesApp API',
+    description='A simple demonstration of a Flask RestPlus powered API',
+    authorizations={
+        'Bearer Auth': {
+            'type': 'apiKey',
+            'in': 'header',
+            'name': 'Authorization'
+        }
     }
-}
+)
 
-# from .services import UserService, RestaurantService, PaymentService, OrderService, OrderItemService, MenuCategoryService, MenuItemService
-# user_service = UserService(db)
-# restaurant_service = RestaurantService(db)
-# payment_service = PaymentService(db)
-# order_service = OrderService(db)
-# order_item_service = OrderItemService(db)
-# menu_category_service = MenuCategoryService(db)
-# menu_item_service = MenuItemService(db)
-
-from .routes import auth_routes, user_routes, restaurant_routes, menu_routes, table_routes
-
-api = Api(app, version='1.0', title='HesApp API', description='A simple demonstration of a Flask RestPlus powered API', authorizations=authorizations)
 api.add_namespace(auth_routes.api, path="/auth")
 api.add_namespace(user_routes.api, path="/user")
 api.add_namespace(restaurant_routes.api, path="/restaurant")
