226e2f568c8e4083d0212c7103c5a905ab6596bc
Booanger <kerndo32@gmail.com>
2023-06-16 20:12:19 +0300

    Add authorization to Swagger UI

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, request, Response
+from werkzeug.security import check_password_hash, generate_password_hash
 from config import Config
 from flask_jwt_extended import JWTManager
 from flask_migrate import Migrate
@@ -24,6 +25,32 @@ db.init_app(app)
 jwt = JWTManager(app)
 migrate = Migrate(app, db)
 
+
+# Swagger UI authentication
+username = app.config["SWAGGER_UI_USERNAME"]
+hashed_password = generate_password_hash(app.config["SWAGGER_UI_PASSWORD"])
+
+
+@app.before_request
+def require_login():
+    if request.path == "/":
+        auth = request.authorization
+        if not auth or not auth.username or not auth.password:
+            return Response(
+                "Login required",
+                401,
+                {"WWW-Authenticate": 'Basic realm="Login required"'},
+            )
+        elif auth.username != username or not check_password_hash(
+            hashed_password, auth.password
+        ):
+            return Response(
+                "Invalid credentials",
+                401,
+                {"WWW-Authenticate": 'Basic realm="Login required"'},
+            )
+
+
 api = Api(
     app,
     version="1.0",
@@ -41,7 +68,5 @@ api.add_namespace(staff_routes.api, path="/staff")
 api.add_namespace(menu_routes.api, path="/menu")
 api.add_namespace(table_routes.api, path="/table")
 api.add_namespace(order_routes.api, path="/order")
-
-# TODO add order routes
-# TODO add payment routes
-# TODO add qr code routes
+# api.add_namespace(payment_routes.api, path="/payment")
+# api.add_namespace(qr_code_routes.api, path="/qr")
--- config.py
+++ config.py
@@ -1,9 +1,13 @@
 import os
 from dotenv import load_dotenv
+import secrets
 
 load_dotenv()
 
+
 class Config(object):
-    SECRET_KEY = os.getenv('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
+    SECRET_KEY = os.getenv("SECRET_KEY") or secrets.token_hex(32)
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URI")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SWAGGER_UI_USERNAME = os.getenv("SWAGGER_UI_USERNAME") or secrets.token_hex(32)
+    SWAGGER_UI_PASSWORD = os.getenv("SWAGGER_UI_PASSWORD") or secrets.token_hex(32)
